4d52ee3248a38a3747f71759792b3ea62e7d90e3
simonrydder <66967123+simonrydder@users.noreply.github.com>
2024-12-12 18:26:57 +0100

    enables save file

--- components/GameButtonView.vue
+++ components/GameButtonView.vue
@@ -10,7 +10,7 @@
           :func="() => roundStore.newRound()"
         ></FunctionalButton>
         <FunctionalButton title="Options" :func="testClick"></FunctionalButton>
-        <FunctionalButton title="Save" :func="testClick"></FunctionalButton>
+        <FunctionalButton title="Save" :func="saveClick"></FunctionalButton>
         <FunctionalButton title="Exit" :func="exitClick"></FunctionalButton>
       </div>
     </div>
@@ -46,6 +46,26 @@ const testClick = () => {
 
 const exitClick = () => {
   router.push("/");
+};
+
+const saveClick = async () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+  const day = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  const timeStamp = `${year}-${month}-${day} ${hours}-${minutes}-${seconds}`;
+
+  const response = await callApi(
+    "/game/save",
+    "POST",
+    { name: timeStamp },
+    "Unable to save game."
+  );
+
+  alert(response.message);
 };
 </script>
 
